207f9a9a7014f65ffc0c0626d07442498cdb3696
yashguptaji <54070427+yashguptaji@users.noreply.github.com>
2021-02-28 16:20:43 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,5 @@
-const express = require('express');
-const router = express.Router();
-const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const express=require('express');
+const router=express.Router();
+router.get('/',(req,res)=> res.send('Welcome'));
 
-// Welcome Page
-router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
-
-// Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('dashboard', {
-    user: req.user
-  })
-);
-
-module.exports = router;
+module.exports=router;
