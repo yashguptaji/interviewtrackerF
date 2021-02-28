0897f073979457d67ceba74fca79628af7689a7a
yashguptaji <54070427+yashguptaji@users.noreply.github.com>
2021-02-28 16:12:23 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://admin:'+ encodeURIComponent('adminkapassword') + '@cluster0.qcwnw.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+
